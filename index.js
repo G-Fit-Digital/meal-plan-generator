55316ee325999dd9f7d012e1917d342ee1610bb0
Joe Sworyk <joesworyk@Joes-MacBook-Pro.local>
2019-09-01 22:06:08 -0400

    <Looking up individual food item>

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const axios = require("axios");
+var cors = require("cors");
 
 mongoose
   .connect("mongodb://localhost/gfit")
@@ -9,6 +10,7 @@ mongoose
   .catch(err => {
     console.log("Not Connecting", err);
   });
+app.use(cors());
 app.use(express.json());
 app.get("/", async (req, res) => {
   res.send("GFIT SERVER RUNNING");
@@ -19,15 +21,7 @@ app.get(`/search/:query`, async (req, res) => {
       `https://api.nal.usda.gov/ndb/search/?format=json&q=${req.params.query}&sort=r&max=100&offset=0&api_key=7gmRblmCoIbJpYoNpXJStU9jR99wHM78S973VXrz&ds=Standard%20Reference`
     )
     .then(response => {
-      res.send(response.data.list.item[0].ndbno);
-      // axios
-      //   .get(
-      //     `https://api.nal.usda.gov/ndb/reports/?ndbno=${response.data.list.item[0].ndbno}&type=s&format=json&api_key=7gmRblmCoIbJpYoNpXJStU9jR99wHM78S973VXrz`
-      //   )
-      //   .then(data => {
-      //     res.send(data.data);
-      //     console.log(data.data);
-      //   });
+      res.send(response.data);
     })
     .catch(err => {
       console.log(err.message);
