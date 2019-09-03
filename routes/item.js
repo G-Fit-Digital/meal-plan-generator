f0c5952b9a0df705972b244d84c9db565b11fc41
Joe Sworyk <joesworyk@Joes-MacBook-Pro.local>
2019-09-02 23:56:05 -0400

    <Updated item model>

--- routes/item.js
+++ routes/item.js
@@ -2,7 +2,7 @@ const { Item } = require("../models/item");
 const express = require("express");
 const router = express.Router();
 
-router.post("/item", async (req, res) => {
+router.post("/", async (req, res) => {
   let item = new Item(req.body);
   await item.save();
   res.send(item);
